2c7193b9d643ba682f5989a1f225e52466f75352
luc-dev10 <luc.dev1008@gmail.com>
2022-05-19 21:46:27 -0400

    refactor naming convention and add category

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -4,8 +4,8 @@ export class Product {
   description: string | undefined;
   price: number | undefined;
   quantity: number | undefined;
-  date_created: Date | undefined;
-  date_updated: Date | undefined;
-  is_active: boolean | undefined;
-  image_url: string | undefined;
+  dateCreated: Date | undefined;
+  dateUpdated: Date | undefined;
+  isActive: boolean | undefined;
+  imageUrl: string | undefined;
 }
